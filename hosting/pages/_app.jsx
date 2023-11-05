888d65d749972c7f8737fb75ea4ca1f61d1ead35
Lewis Taylor <Lewis101taylor@gmail.com>
2023-11-05 14:01:38 +0000

    Removed unnecessary head item

--- hosting/pages/_app.jsx
+++ hosting/pages/_app.jsx
@@ -1,13 +1,9 @@
-import Head from "next/Head";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
     </>
   );
 }
